226503d1d9550790d561062db641bde42976a106
mpv <mattiapivi@gmail.com>
2021-10-18 01:24:09 +0200

    session_01 done

--- Solutions/Session_01/04_Units of measure.cpp
+++ Solutions/Session_01/04_Units of measure.cpp
@@ -5,4 +5,15 @@
 1 inch
 0.0254 m
 ```
-/*
+*/
+#include <iostream>
+
+int main() 
+{
+double insert {};
+constexpr double i2m{0.0254};
+std::cout << "Please insert the n° of inch(es) you want to convert to meter(s)\n";
+std::cin >> insert;
+
+std::cout << insert << " inch(es) is/are equal to " << (insert*i2m) << " meter(s)" << std::endl;
+}
