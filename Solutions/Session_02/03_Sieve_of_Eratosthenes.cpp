410b8558c9854c3e47c178283953c9c6a0367b63
mpv <mattiapivi@gmail.com>
2021-10-18 22:49:27 +0200

    solutions2_take0

---
+++ Solutions/Session_02/03_Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,32 @@
+/*
+- Implement the Sieve of Eratosthenes algorithm. Write a code that prints on screen the first prime numbers up to `N` **included**, where `N` is read from stdin. The first printed number is `2`.
+
+```shell
+$ ./a.out 
+insert number: 5
+2
+3
+5
+
+$ ./a.out
+insert number: 10
+2
+3
+5
+7
+```
+
+*Hints*:
+- https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes :)
+
+- the function `std::sqrt`, which computes the square root of a number, is defined in `<cmath>`
+```c++
+#include <cmath>
+...
+std::cout << std::sqrt(4) << std::endl;
+```
+*/
+int main (){
+
+}
+
---
+++ Solutions/Session_02/04_Use_std_vector.cpp
@@ -0,0 +1,8 @@
+/*
+Re-implement the exercises *Prime numbers* and *Sieve of Eratosthenes* using `std::vector` instead of built-in arrays. `std::vector` is defined in the header `<vector>`.
+*/
+
+int main (){
+
+}
+
