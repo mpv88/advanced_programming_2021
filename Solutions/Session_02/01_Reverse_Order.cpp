9f6360fa8dc1e904deadce21197128d61a9df4fd
mpv <mattiapivi@gmail.com>
2021-10-18 22:49:27 +0200

    solutions2_take0

---
+++ Solutions/Session_02/01_Reverse_Order.cpp
@@ -0,0 +1,22 @@
+/*
+- Write a program that reads from stdin the length of an array and then stores in it the numbers given from stdin. 
+  The program should then print the numbers in **reverse order**. Remember to free the used memory properly. Divide the problem into two parts:
+
+  1. Write one **template** function that allocates on the *heap* one array of a given size, initializes its elements, and returns the pointer to the first element.
+
+  2. Write a **template** function that prints the elements of an array in the reverse order.
+
+- Test with integers and doubles.
+
+- Use `std::size_t`.
+
+*Hints*: 
+
+- If you have to pass an array to a function, pass the pointer to its first element and the size of the array itself.
+
+- Remember to check your code with `valgrind`. If you have a MAC, you might have nasty surprises. Run `valgrind` on a **Linux**  system. Alternatively, compile and link your code with the following flags `-g -fsanitize=address`  and then run your executable. If it runs fine, you have no memory leaks. If it doesn't, you have some.
+*/
+int main (){
+
+}
+
---
+++ Solutions/Session_02/02_Prime_Numbers.cpp
@@ -0,0 +1,21 @@
+/*
+- Populate an array named `primes` containing the first prime numbers smaller than 100. Consider 2 as the first prime (i.e. `primes[0] == 2`).
+  
+  - you are free to choose the design and the implementation.
+
+  - please comment and motivate your choices (e.g., allocate one big
+    array, or every time I find a new prime number I increase the size of
+    the previous `primes` array, etc.)
+
+*Hints*:
+
+- The remainder of a division between two integers is computed as follows:
+```c++
+unsigned int remainder = 6%3;
+```
+- Inside a for loop, you can use `continue` to immediately go to the next iteration, `break` to exit the loop. Note that if you have two or more nested loops, the statements affect only the innermost loop.
+*/
+int main (){
+
+}
+
