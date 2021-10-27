b7322e2bf4fd07f4b5833f9ca04dacd065e1d9ea
mpv <mattiapivi@gmail.com>
2021-10-28 00:15:40 +0200

    solutions3_take2

--- Solutions/Session_03/04_Mean_and_Median.cpp
+++ Solutions/Session_03/04_Mean_and_Median.cpp
@@ -1,4 +1,6 @@
-/* c++ 04_Mean_and_Median.cpp -o 04_Mean_and_Median -Wall -Wextra -g -std=c++14
+/* COMPILE WITH: c++ 04_Mean_and_Median.cpp -o 04_Mean_and_Median -Wall -Wextra -g -std=c++14
+   DEBUG WITH: gdb ./04_Mean_and_Median core; (gdb) r; (gdb) quit;
+   TEST WITH: g++ -g -fsanitize=address 04_Mean_and_Median.cpp; ./04_Mean_and_Median;
 - Store the numbers contained in file `temperatures.txt` into a `std::vector<double>` and compute:
   - the mean
   - the median
@@ -9,41 +11,55 @@
 - you can **sort** the elements of a vector `v` as follows
 ```c++
 #include <algorithm>
-
 std::vector<double> v;
 ...
 std::sort( v.begin(), v.end() );
 ```
 */
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
-template <typename ia>
-ia * init(const ia input[], std::size_t n)
-{ 
-  ia * ar{new ia[n]};
-  for (std::size_t i=0; i<n; i++)
-    *(ar+i) = *(input+i);
-  return ar;
+template <typename T>
+T mean(const std::vector<T>& input)
+{
+  T sum{0};
+  for (std::size_t i=0; i<input.size(); i++)
+    sum += input[i];
+  return sum/input.size();
 }
 
-template <typename rv>
-void reverse(const rv * ar, std::size_t n)
+template <typename T>
+T median(std::vector<T>& input) //no const to sort
 { 
-  for (int i=n; i>=0; --i)
-    std::cout << ar[i] << std::endl;
-  delete[] ar;
+  std::sort(input.begin(),input.end());
+  if (input.size() % 2 == 0)
+   return (input[input.size()/2-1]+input[input.size()/2])/2; //even v
+  else return input[input.size()/2+1]; //odd v
+}
+
+template <typename T>
+void read_file(const std::string file_name, std::vector<T>& v) //no const to fill
+{ 
+  T number{};
+  std::ifstream input_file(file_name, std::ios::in);
+  if (!input_file) 
+  {
+   std::cerr << "Can't open " << file_name << " file!" << std::endl;
+   exit(1); //exits file
+  }
+  while (input_file >> number) //read lines as doubles
+   v.emplace_back(number);
 }
 
 int main ()
 {
-  std::string input;
-  std::size_t n{};
-  std::cout << "Please insert your integers on one line: ";
-  std::getline(std::cin, input);
-  n=input.length();
-  auto * ptr=init<char>(input.c_str(), n);  
-  reverse<char>(ptr, n);
+  std::string file_name = "temperatures.txt"; 
+  std::vector<double> v{};
+  read_file(file_name, v);
+  std::cout << "The mean temperature is " << mean(v) << " C°" << std::endl;
+  std::cout << "The median temperature is " << median(v) << " C°" << std::endl;
   return 0;
 }
-
