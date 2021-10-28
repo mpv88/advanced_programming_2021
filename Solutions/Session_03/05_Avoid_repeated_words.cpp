421a97efa79ccc1aeb1a2b0b3467e229495792e6
mpv <mattiapivi@gmail.com>
2021-10-28 21:38:30 +0200

    solutions3_take3

--- Solutions/Session_03/05_Avoid_repeated_words.cpp
+++ Solutions/Session_03/05_Avoid_repeated_words.cpp
@@ -1,38 +1,30 @@
-/* c++ 05_Avoid_repeated_words.cpp -o 05_Avoid_repeated_words -Wall -Wextra -g -std=c++14
+/* COMPILE WITH: c++ 05_Avoid_repeated_words.cpp -o 05_Avoid_repeated_words -Wall -Wextra -g -std=c++14
+   DEBUG WITH: gdb ./05_Avoid_repeated_words core; (gdb) r; (gdb) quit;
+   TEST WITH: g++ -g -fsanitize=address 05_Avoid_repeated_words.cpp -o 05_Avoid_repeated_words; ./05_Avoid_repeated_words;
 - Read from stdin a sequence of words. Store each word in a `std::vector<std::string>`. Then, print the words without repetitions.
 
 *Hints*:
 - you can print the words in any order you want (i.e., you are not required to print the words in the order you have read them)
 */
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
 
-template <typename ia>
-ia * init(const ia input[], std::size_t n)
-{ 
-  ia * ar{new ia[n]};
-  for (std::size_t i=0; i<n; i++)
-    *(ar+i) = *(input+i);
-  return ar;
-}
-
-template <typename rv>
-void reverse(const rv * ar, std::size_t n)
-{ 
-  for (int i=n; i>=0; --i)
-    std::cout << ar[i] << std::endl;
-  delete[] ar;
-}
-
-int main ()
+int main() 
 {
-  std::string input;
-  std::size_t n{};
-  std::cout << "Please insert your integers on one line: ";
-  std::getline(std::cin, input);
-  n=input.length();
-  auto * ptr=init<char>(input.c_str(), n);  
-  reverse<char>(ptr, n);
-  return 0;
+ std::string line;
+ std::vector<std::string> v{};
+ std::cout << "Please insert one input for each line:" << std::endl;
+ while(std::getline(std::cin,line))
+ {
+  if(line==".") break;
+  v.push_back(line);
+ }
+ std::sort(v.begin(),v.end());
+ for(std::size_t i=0; i<v.size(); i++)
+  if(v[i]==v[i+1])
+   continue;
+  else
+   std::cout << v[i] << std::endl;
 }
-
