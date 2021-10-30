bc8715344ec6efe0aa3f615cd1573b4b11a108ff
mpv <mattiapivi@gmail.com>
2021-10-31 00:07:39 +0200

    session_03 done

--- Solutions/Session_03/06_Use_Std_Map.cpp
+++ Solutions/Session_03/06_Use_Std_Map.cpp
@@ -1,6 +1,7 @@
 /* COMPILE WITH: c++ 06_Use_Std_Map.cpp -o 06_Use_Std_Map -Wall -Wextra -g -std=c++14 -O3
    DEBUG WITH: gdb ./06_Use_Std_Map core; (gdb) r; (gdb) quit;
    TEST WITH: g++ -g -fsanitize=address 06_Use_Std_Map.cpp -o 06_Use_Std_Map; ./06_Use_Std_Map;
+   TEST WITH: valgrind --leak-check=full --track-origins=yes ./06_Use_Std_Map;
 - Read the `LittleWomen.txt` and then print all the read words (without repetitions) followed by the number of repetitions of that word. Compare the time to do the same using `std::vector`, `std::map`, `std::unordered_map`. The order in which the words are printed is **not** relevant.
 
 - You can measure the elapsed time as follows:
@@ -27,78 +28,105 @@ auto t0 = high_resolution_clock::now();
 #include <algorithm>
 #include <chrono>
 #include <fstream>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <map>
-#include <regex>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 template <typename T>
-void file_to_map(const T file_name, std::map<T, unsigned int>& m)
+void file_to_map(std::ifstream& input_file, std::map<T,unsigned int>& m)
 { 
-  const std::regex re{ "\\w+" };
-  std::string line;
-  std::ifstream input_file(file_name, std::ios::in);
-  if (!input_file) 
-  {
-   std::cerr << "Can't open " << file_name << " file!" << std::endl;
-   exit(1); //exits file
-  }
-  while (input_file >> line)
-  {
-   for(auto word{ std::sregex_token_iterator(line.begin(),line.end(),re) }; word != std::sregex_token_iterator(); ++word)
-    m[*word]++;
-
-   for(const auto& [word, count] : m) 
-    std::cout << std::setw(20) << word << "\toccurred\t" << count << " times\n";
-  }
+  auto t0 = std::chrono::high_resolution_clock::now();//starts clock
+  
+  for (std::string line; input_file>>line;)
+   {
+    line.erase(std::remove_if(line.begin(), line.end(), ispunct), line.end());//cleans text
+    ++m[line];
+   }
+  for (const auto& element : m)
+   std::cout << element.first << " " << element.second << std::endl;//prints key-value pairs
+  
+  auto t1 = std::chrono::high_resolution_clock::now();//stops clock
+  auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0);
+  std::cout << "Elapsed " << elapsed.count() << " [milliseconds]" << std::endl; 
 }
 
 template <typename T>
-void file_to_vector(const T file_name, std::vector<T>& v)
+void file_to_unordered_map(std::ifstream& input_file, std::unordered_map<T,unsigned int>& u_m)
 { 
-  auto t0 = std::chrono::high_resolution_clock::now();
-  std::size_t counter{1};
+  auto t0 = std::chrono::high_resolution_clock::now();//starts clock
+  
+  for (std::string line; input_file>>line;)
+   {
+    line.erase(std::remove_if(line.begin(), line.end(), ispunct), line.end());//cleans text
+    ++u_m[line];
+   }
+  for (const auto& element : u_m)
+   std::cout << element.first << " " << element.second << std::endl;//prints key-value pairs
+  
+  auto t1 = std::chrono::high_resolution_clock::now();//stops clock
+  auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0);
+  std::cout << "Elapsed " << elapsed.count() << " [milliseconds]" << std::endl; 
+}
+
+template <typename T>
+void file_to_vector(std::ifstream& input_file, std::vector<T>& v)
+{ 
+  auto t0 = std::chrono::high_resolution_clock::now();//starts clock
+  std::size_t counter{};
   std::string line;
-  std::ifstream input_file(file_name, std::ios::in);
-  if (!input_file) 
-  {
-   std::cerr << "Can't open " << file_name << " file!" << std::endl;
-   exit(1); //exits file
-  }
   while (input_file >> line)
   {
-   line.erase(std::remove_if(line.begin(), line.end(), ispunct), line.end());
-   v.emplace_back(line);
+   line.erase(std::remove_if(line.begin(), line.end(), ispunct), line.end());//cleans text 
+   v.emplace_back(line);//fills vector
   }
-
-  std::sort(v.begin(),v.end());
+  std::sort(v.begin(),v.end());//sorts vector
   for(std::size_t i=0; i<v.size(); i++)
    {
     if(v[i]==v[i+1])
-    {
-     counter++;
-     continue;
-    }
+     ++counter;
     else
     {
      std::cout << counter << " " << v[i] << std::endl;
      counter=1;
     }
-   }
-  auto t1 = std::chrono::high_resolution_clock::now();
+  }
+  auto t1 = std::chrono::high_resolution_clock::now();//stops clock
   auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0);
   std::cout << "Elapsed " << elapsed.count() << " [milliseconds]" << std::endl; 
 }
 
 int main ()
 {
-  std::string file_name = "LittleWomen.txt"; 
-  std::vector<std::string> v{};
-  std::map<std::string, unsigned int> m {};
-  //file_to_vector(file_name, v);
-  file_to_map(file_name, m);
+  std::string file_name {"LittleWomen.txt"};//.txt source
+  std::vector<std::string> v{}; //target vector
+  std::map<std::string,unsigned int> m {}; //target map
+  std::unordered_map<std::string,unsigned int> u_m {};//target unordered map
+
+  std::ifstream input_file(file_name, std::ios::in);//opens file for input ops
+  if (!input_file) 
+  {
+   std::cerr << "Can't open " << file_name << " file!" << std::endl;
+   exit(1); //exits file
+  }
+  else
+  {
+  file_to_map(input_file, m);//1_read to map
+  	//
+  input_file.clear(); //clear stream flags & error state
+  input_file.seekg(0, std::ios::beg);//back to the beginning of .txt
+	//
+  file_to_unordered_map(input_file, u_m);//2_read to unordered map
+	//
+  input_file.clear(); //clear stream flags & error state
+  input_file.seekg(0, std::ios::beg);//back to the beginning of .txt
+	//
+  file_to_vector(input_file, v);//3_read to vector
+  	//
+  input_file.close();//closes file
+  }
   return 0;
 }
 
